b935df1b1432e5d9160b3c488e72886745e9c598
vinistrauss <vini.strauss@hotmail.com>
2020-03-29 20:00:29 -0300

    first commit

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const connection = require("../../src/database/connection");
+
+describe("ONG", () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("should be able to create a new ONG", async () => {
+    const response = await request(app)
+      .post("/ongs")
+      .send({
+        name: "ViniciusStrauss",
+        email: "vini@email.com",
+        whatsapp: "9074774747",
+        city: "Santos",
+        uf: "SC"
+      });
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toHaveLength(8);
+  });
+});
